9152fb2e35a63a62faca2e0a9c82f199ff2158ac
jxhnlcs <joohnzay@gmail.com>
2023-09-09 21:02:57 -0300

    finish

--- blog/src/app/data/dataFake.ts
+++ blog/src/app/data/dataFake.ts
@@ -1,14 +1,27 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "Danganronpa: criador revela estar aberto a reviver a franquia",
+    "description": "Kazutaka Kodaka preferiu começar um novo projeto, mas não descarta um retorno à consagrada série de assassinato.",
+    "photoCover":"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgp4z2c-yUvsSn8EBeKeBI-D_b1e4NjFW1Jaeb20n28ueB6_a6eDOexILHWAhiW431qn8UputxqOVRGS7hlE_uJdbEamDP60gEw5K9MxKxTwcjLVMT7DEfzLzRw8riyKWF98Y87c7bc6gRwtZeERK52HxFcUy2f_VAkW6eSIQYWPtpeJHpHEB5u6Ag0/w640-h404/danganronpa%20kyoko%20makoto.jpeg"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "One Piece: 2ª temporada ganha a maior notícia de todas",
+    "description": "Após sua estreia de grande impacto, os produtores por trás de One Piece da Netflix afirmam a maior notícia que a série poderia receber: a de que os roteiros para uma possível segunda temporada já estão completos. Além disso, que a próxima temporada poderá estar “pronta para ser exibida” em um ano, assim que a produção começar.",
+    "photoCover":"https://mixdeseries.com.br/wp-content/uploads/2023/09/One-Piece-2-temporada-da-serie-noticia.webp"
+  },
+  {
+    "id":"3",
+    "title": "Shingeki no Kyojin – Novo projeto será anunciado em Outubro",
+    "description": "Sendo assim, a confirmação desse novo anúncio, se deu através de Kawakubo Shintaro, o editor de Shingeki no Kyojin que informou em seu twitter, e marcou o arroba do novo projeto @shingeki_FLY ( Shingeki no Kyojin Fly ).",
+    "photoCover":"https://www.supergeek.cl/noticias/site/artic/20230303/imag/foto_0000000420230303133236/attack_p3.jpg"
+  },
+  {
+    "id":"4",
+    "title": "No dia 24 de setembro teremos o evento Anime Fest em Santana (SP)",
+    "description": "São Paulo Anime Fest é um dos eventos mais reconhecidos aqui no Brasil e também é considerado o maior evento do interior paulista, o evento irá acontecer no dia 24 de setembro de 2023 em Santana (Rua Dom Henrique Mourão, 201, Santana, São Paulo, SP). Consigo irá trazer diversas atrações interessantes como sempre vão trazendo a anos como k-pop, cosplayers, karaoke, dubladores e influenciadores alguns deles são Felipe Sieira do canal Chapéus de Palha, o famoso youtuber Rato Borrachudo e também Bruno Bandeira criador de conteúdos e diversos outras personalidades que agregam na nosso cultura otaku.",
+    "photoCover":"https://www.animeunited.com.br/oomtumtu/2023/09/anime-fest.jpg"
   }
+  
 ]
